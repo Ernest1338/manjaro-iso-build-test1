f063d55a3d2fb7274dbd9f8fa6747f623928647d
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 17:38:54 +0200

    Update DDMJ_updates.sh

--- DDMJOS/Install_Scripts/DDMJ_updates.sh
+++ DDMJOS/Install_Scripts/DDMJ_updates.sh
@@ -2,7 +2,7 @@
 
 current_version=$(cat ~/DDMJ/version)
 update_version=$(curl -s https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/version)
-if [ $current_version == $update_version ]
+if [ $current_version -lt $update_version ]
 then
     notify-send --expire-time=30000 --icon="alienarena" --app-name="DDMJ OS Updater" "Update available - New version: v$update_version.0" "Starting an update in 30 seconds"
     sleep 30
