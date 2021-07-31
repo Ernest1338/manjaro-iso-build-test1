e37b573ab2e8cc40d313d07f7efb70f660e9ac3f
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 16:06:13 +0200

    Update DDMJ_updates.sh

--- DDMJOS/Install_Scripts/DDMJ_updates.sh
+++ DDMJOS/Install_Scripts/DDMJ_updates.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+current_version=$(cat ~/DDMJ/version)
+update_version=$(curl -s https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/version)
+echo $current_version
+echo $update_version
