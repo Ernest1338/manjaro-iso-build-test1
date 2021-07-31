3a466cae58035107449d929a61de085efb9984a8
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 19:42:06 +0200

    Update DDMJ.sh

--- DDMJOS/Install_Scripts/DDMJ.sh
+++ DDMJOS/Install_Scripts/DDMJ.sh
@@ -4,7 +4,7 @@
 sed -i 's/firefox/brave-browser/' ~/.config/plasma-org.kde.plasma.desktop-appletsrc
 
 # Setting up wallpaper
-sudo mkdir /usr/share/wallpapers/DDMJ
+kdesu mkdir /usr/share/wallpapers/DDMJ
 sudo wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/1920x1080.png -O /usr/share/wallpapers/DDMJ/1920x1080.png
 plasma-apply-wallpaperimage /usr/share/wallpapers/DDMJ/1920x1080.png
 
