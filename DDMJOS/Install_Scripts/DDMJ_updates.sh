9f845a97a4df512ad044ad34fc57ec7f1dd001e4
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 16:58:11 +0200

    Update DDMJ_updates.sh

--- DDMJOS/Install_Scripts/DDMJ_updates.sh
+++ DDMJOS/Install_Scripts/DDMJ_updates.sh
@@ -4,8 +4,7 @@ current_version=$(cat ~/DDMJ/version)
 update_version=$(curl -s https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/version)
 if [ $current_version -lt $update_version ]
 then
-    notify-send --icon="alienarena" --app-name="DDMJ OS Updater" "Update available
-New version: v$update_version.0"
+    notify-send --icon="alienarena" --app-name="DDMJ OS Updater" "Update available" "New version: v$update_version.0"
 else
     notify-send --icon="alienarena" --app-name="DDMJ OS Updater" "Everything is up-to-date"
 fi
