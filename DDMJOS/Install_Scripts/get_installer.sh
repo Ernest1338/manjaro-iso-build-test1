528cc719e174e405d45a7ab7f40e84c37c686a26
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 13:21:27 +0200

    Update get_installer.sh

--- DDMJOS/Install_Scripts/get_installer.sh
+++ DDMJOS/Install_Scripts/get_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# To quickly get and run: curl -sSf https://tinyurl.com/ddmjos1 | bash
+# To quickly get and run: curl -sSfL https://tinyurl.com/ddmjos1 | bash
 
 # Getting the install scripts and placing them in the proper directories
 wget https://raw.githubusercontent.com/Ernest12338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/DDMJ.sh -O ~/Downloads/
