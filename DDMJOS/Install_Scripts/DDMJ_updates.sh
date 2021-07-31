ce809123cc3dd8d6a632afff2a5f88151e8a31a4
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 17:52:46 +0200

    Update DDMJ_updates.sh

--- DDMJOS/Install_Scripts/DDMJ_updates.sh
+++ DDMJOS/Install_Scripts/DDMJ_updates.sh
@@ -7,6 +7,10 @@ then
     notify-send --expire-time=30000 --icon="alienarena" --app-name="DDMJ OS Updater" "Update available - New version: v$update_version.0" "Starting an update in 30 seconds"
     sleep 30
     notify-send --icon="alienarena" --app-name="DDMJ OS Updater" "Updating..."
+    wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/DDMJ.sh -O ~/DDMJ/DDMJ.sh
+    sudo chmod +x ~/DDMJ/DDMJ.sh
+    bash ~/DDMJ/DDMJ.sh
+    notify-send --icon="alienarena" --app-name="DDMJ OS Updater" "Update complete"
 else
     notify-send --icon="alienarena" --app-name="DDMJ OS Updater" "Everything is up-to-date"
 fi
