43e416522a1fbe7cf2138efb17837afc1dc33ba7
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 19:53:39 +0200

    Update DDMJ.sh

--- DDMJOS/Install_Scripts/DDMJ.sh
+++ DDMJOS/Install_Scripts/DDMJ.sh
@@ -4,8 +4,7 @@
 sed -i 's/firefox/brave-browser/' ~/.config/plasma-org.kde.plasma.desktop-appletsrc
 
 # Setting up wallpaper
-kdesu mkdir /usr/share/wallpapers/DDMJ
-sudo wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/1920x1080.png -O /usr/share/wallpapers/DDMJ/1920x1080.png
+kdesu -c "mkdir /usr/share/wallpapers/DDMJ; wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/1920x1080.png -O /usr/share/wallpapers/DDMJ/1920x1080.png"
 plasma-apply-wallpaperimage /usr/share/wallpapers/DDMJ/1920x1080.png
 
 # Setupping DDMJ directory if it doesn't exist yet
@@ -20,9 +19,9 @@ lookandfeeltool -a org.kde.breezedark.desktop
 
 # Getting the update checker and placing it on the desktop
 wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/Check%20for%20DDMJ%20OS%20updates.desktop -O ~/Desktop/"Check for DDMJ OS updates.desktop"
-sudo chmod +x ~/Desktop/"Check for DDMJ OS updates.desktop"
+chmod +x ~/Desktop/"Check for DDMJ OS updates.desktop"
 wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/DDMJ_updates.sh -O ~/DDMJ/DDMJ_updates.sh
-sudo chmod +x ~/DDMJ/DDMJ_updates.sh
+chmod +x ~/DDMJ/DDMJ_updates.sh
 
 # Configuring KWIN
 kwriteconfig5 --file $HOME/.config/kwinrc --group Plugins --key contrastEnabled "True"
@@ -39,8 +38,8 @@ rm ~/Desktop/"Transform into DDMJ OS.desktop"
 rm ~/DDMJ/DDMJ.sh
 
 # Restarting plasmashell
-pkill plasmashell
-plasmashell &
+killall plasmashell
+kstart5 plasmashell
 
 # Welcome user
 sleep 10
