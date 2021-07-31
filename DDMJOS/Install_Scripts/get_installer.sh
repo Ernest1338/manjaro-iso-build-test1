201613b0a6d0cd1d2fb4b52cb2fa5619894a415c
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 13:44:12 +0200

    Update get_installer.sh

--- DDMJOS/Install_Scripts/get_installer.sh
+++ DDMJOS/Install_Scripts/get_installer.sh
@@ -2,7 +2,10 @@
 
 # To quickly get and run: curl -sSfL https://tinyurl.com/ddmjos1 | bash
 
+# Making DDMJ directory if it doesn't yet exists
+mkdir ~/DDMJ
+
 # Getting the install scripts and placing them in the proper directories
-wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/DDMJ.sh -O ~/Downloads/DDMJ.sh
+wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/DDMJ.sh -O ~/DDMJ/DDMJ.sh
 wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/Transform%20into%20DDMJ%20OS.desktop -O ~/Desktop/"Transform into DDMJ OS.desktop"
 sudo chmod +x ~/Desktop/"Transform into DDMJ OS.desktop"
