0d26524cbcfc2aa7813a6e4954d35fe99cb7b8c4
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2021-07-31 13:28:12 +0200

    Update get_installer.sh

--- DDMJOS/Install_Scripts/get_installer.sh
+++ DDMJOS/Install_Scripts/get_installer.sh
@@ -5,3 +5,4 @@
 # Getting the install scripts and placing them in the proper directories
 wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/DDMJ.sh -O ~/Downloads/DDMJ.sh
 wget https://raw.githubusercontent.com/Ernest1338/manjaro-iso-build-test1/main/DDMJOS/Install_Scripts/Transform%20into%20DDMJ%20OS.desktop -O ~/Desktop/"Transform into DDMJ OS.desktop"
+sudo chmod +x ~/Desktop/"Transform into DDMJ OS.desktop"
